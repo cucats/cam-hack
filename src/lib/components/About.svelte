01955ea25803b2299c412c0fd1bba7c1b2edcab6
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-10-16 17:28:26 +0100

    Format

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -5,6 +5,7 @@
     of Cambridge. Over an action-packed weekend, students come together to
     design and build technology projects from the ground up. We welcome
     participants ranging from curious beginners to seasoned coders. Expect
-    plenty of free stash, food, and mini-events throughout this hackathon. Prizes TBD.
+    plenty of free stash, food, and mini-events throughout this hackathon.
+    Prizes TBD.
   </p>
 </div>
